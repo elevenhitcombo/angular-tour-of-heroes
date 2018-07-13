8cebfe68e9ebf2b29c613df0f96eb8088c8f17bd
CarolMSmith <carolmsmith777@gmail.com>
2018-07-13 16:07:09 -0400

    Pushing baseline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
+// import Component functionality from @angular/core
 import { Component } from '@angular/core';
 
 @Component({
+  // 
   selector: 'app-root',
+  // Specifies the main markup file
   templateUrl: './app.component.html',
+  // Specifies the styles for the component
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // Member variable
+  title = 'Angular Tour Of Heroes';
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: "Rorschach"},
+    {id: 2, name: "Thor"},
+    {id: 3, name: "Iron Man"},
+    {id: 4, name: "Superman"},
+    {id: 5, name: "Batman"},
+    {id: 6, name: "Bananaman"},
+    {id: 7, name: "Hulk"},
+    {id: 8, name: "Black Widow"},
+    {id: 9, name: "Captain America"},
+    {id: 10, name: "Jim Bob Walton in ill-fitting spandex"}
+];
